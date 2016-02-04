da62799eebae143a82d03c81b28a6cf840d98cb0
549995358_m <549995358@qq.com>
2016-02-04 15:13:58 +0800

    添加服务端口配置

--- src/main/java/com/xjkwq1qq/core/ThriftServiceApplication.java
+++ src/main/java/com/xjkwq1qq/core/ThriftServiceApplication.java
@@ -1,6 +1,8 @@
 package com.xjkwq1qq.core;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.xjkwq1qq.annotation.ThriftService;
 
@@ -22,6 +26,8 @@ public class ThriftServiceApplication {
 	public static final Logger LOG = LoggerFactory.getLogger(ThriftServiceApplication.class);
 
 	private ApplicationContext applicationContext;
+	public static final String DEFAULT_CONFIGSOURCE = "/thrift-spring.properties";
+	private String configSource;
 
 	@Autowired
 	public void setApplicationContext(ApplicationContext applicationContext) {
@@ -38,7 +44,8 @@ public class ThriftServiceApplication {
 			return;
 		}
 
-		TServerTransport serverTransport = new TServerSocket(9090);
+		int port = getPort();
+		TServerTransport serverTransport = new TServerSocket(port);
 		TMultiplexedProcessor multiplexedProcessor = new TMultiplexedProcessor();
 		TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(multiplexedProcessor));
 		for (Map.Entry<String, Object> entry : thriftServices.entrySet()) {
@@ -53,11 +60,48 @@ public class ThriftServiceApplication {
 			// 获取processor
 			TProcessor processor = ThriftUtil.buildProcessor(value);
 			// 注册
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("注册服务组件：" + name + "," + processor.getClass());
+			}
 			multiplexedProcessor.registerProcessor(name, processor);
 		}
 
 		// 启动
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("启动服务，监听端口：" + port);
+		}
 		server.serve();
+	}
+
+	/**
+	 * 获取thrift启动端口
+	 * 
+	 * @return thrift启动端口
+	 * @throws IOException
+	 */
+	private int getPort() throws IOException {
+		Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource(getConfigSource()));
+		int port = 9090;
+		try {
+			String thriftPort = properties.getProperty("thrift.port");
+			if (StringUtils.isNotBlank(thriftPort)) {
+				port = Integer.valueOf(thriftPort);
+			}
+		} catch (Exception e) {
+
+		}
+		return port;
+	}
+
+	private String getConfigSource() {
+		if (StringUtils.isBlank(configSource)) {
+			configSource = DEFAULT_CONFIGSOURCE;
+		}
+		return configSource;
+	}
+
+	public void setConfigSource(String configSource) {
+		this.configSource = configSource;
 	}
 
 }

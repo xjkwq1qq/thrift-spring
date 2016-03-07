47369ea941b945cc0bf8053059a535859ed09eed
549995358_m <549995358@qq.com>
2016-03-07 16:18:49 +0800

    提交关闭事件及无配置默认端口

--- src/main/java/com/xjkwq1qq/core/ThriftServiceApplication.java
+++ src/main/java/com/xjkwq1qq/core/ThriftServiceApplication.java
@@ -17,17 +17,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.xjkwq1qq.annotation.ThriftService;
 
-public class ThriftServiceApplication {
+public class ThriftServiceApplication implements ApplicationContextAware, ApplicationListener<ContextClosedEvent> {
 	public static final Logger LOG = LoggerFactory.getLogger(ThriftServiceApplication.class);
 
 	private ApplicationContext applicationContext;
 	public static final String DEFAULT_CONFIGSOURCE = "/thrift-spring.properties";
 	private String configSource;
+	private TServer server;
 
 	@Autowired
 	public void setApplicationContext(ApplicationContext applicationContext) {
@@ -47,7 +51,7 @@ public class ThriftServiceApplication {
 		int port = getPort();
 		TServerTransport serverTransport = new TServerSocket(port);
 		TMultiplexedProcessor multiplexedProcessor = new TMultiplexedProcessor();
-		TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(multiplexedProcessor));
+		server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(multiplexedProcessor));
 		for (Map.Entry<String, Object> entry : thriftServices.entrySet()) {
 			String name = entry.getKey();
 			Object value = entry.getValue();
@@ -74,15 +78,23 @@ public class ThriftServiceApplication {
 	}
 
 	/**
+	 * closed event
+	 */
+	@Override
+	public void onApplicationEvent(ContextClosedEvent event) {
+		server.stop();
+	}
+
+	/**
 	 * 获取thrift启动端口
 	 * 
 	 * @return thrift启动端口
 	 * @throws IOException
 	 */
 	private int getPort() throws IOException {
-		Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource(getConfigSource()));
 		int port = 9090;
 		try {
+			Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource(getConfigSource()));
 			String thriftPort = properties.getProperty("thrift.port");
 			if (StringUtils.isNotBlank(thriftPort)) {
 				port = Integer.valueOf(thriftPort);
